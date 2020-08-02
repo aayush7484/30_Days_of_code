451899f984ac14f49bcbcc3308debad7ebbf75ea
Aayush Kumar <aayush14140151@gmail.com>
2020-08-02 06:20:31 +0530

    final

---
+++ Day9.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n<0:
+        print("Not Valid Input")
+    elif n==0 or n==1:
+        return n
+    else:
+        return n*factorial(n-1)
+n=int(input())
+print(factorial(n))
